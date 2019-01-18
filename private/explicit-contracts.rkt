6c329e1102caf1647c3de542f0e51f9831f58abd
Cameron Moy <mail@camoy.name>
2019-01-18 12:32:04 -0500

    Add skeleton for intercepting contracts

--- private/explicit-contracts.rkt
+++ private/explicit-contracts.rkt
@@ -1,12 +1,15 @@
 #lang racket
 
 (require racket/cmdline)
+
+(define tr-provide-contracts (make-parameter (void)))
+(define tr-require-contracts (make-parameter (void)))
 (define in-place (make-parameter #f))
 
 (module+ main
   (define targets
     (command-parse (current-command-line-arguments)))
-  
+
   (unless (andmap file-exists? targets)
     (define unknown-file
       (findf (negate file-exists?) targets))
@@ -15,6 +18,14 @@
   (define tr-targets
     (filter typed-module? targets))
 
+  (for-each explicit-contracts tr-targets))
+
+(define (explicit-contracts target)
+  (define provide-hash (make-hash))
+  (define require-hash (make-hash))
+  (parameterize ([tr-provide-contracts provide-hash]
+                 [tr-require-contracts require-hash])
+    target))
 
 (define (command-parse argv)
   (command-line
