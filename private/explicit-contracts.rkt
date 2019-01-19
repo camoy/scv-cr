79fb2946838ccd5dafb69919f84503ae16d6e968
Cameron Moy <mail@camoy.name>
2019-01-18 19:05:59 -0500

    Fix communication of contract data
    Mutating boxes that were provided by the module from within TR
    was not working. I have a feeling this has to do with phase level.
    TR might be at a higher phase level than the module when run from
    raco, preventing communication. I'm not entirely sure though. In
    any case the nasty hack to fix this is to communicate via files
    by serializing all the data. That's what I've done unless I find
    a better solution (or determine there was some other root cause).

--- private/explicit-contracts.rkt
+++ private/explicit-contracts.rkt
@@ -1,11 +1,13 @@
 #lang racket
 
-(require racket/cmdline)
+(require racket/cmdline
+         racket/serialize)
+
 (provide intercept-require-contracts
          intercept-provide-contracts)
 
-(define require-contracts (make-parameter (box #f)))
-(define provide-contracts (make-parameter (box #f)))
+(define require-contracts-file "_require-contracts.dat")
+(define provide-contracts-file "_provide-contracts.dat")
 (define in-place (make-parameter #f))
 
 (module+ main
@@ -17,30 +19,54 @@
       (findf (negate file-exists?) targets))
     (raise-user-error 'explicit-contracts "could not find ~s" unknown-file))
 
-  (define tr-targets
-    (filter typed-module? targets))
-
-  (for-each explicit-contracts tr-targets))
+  (for-each explicit-contracts targets))
 
 (define (explicit-contracts target)
-  (define require-hash (box '()))
-  (define provide-hash (box '()))
-  (parameterize ([require-contracts require-hash]
-                 [provide-contracts provide-hash])
-    target))
+  (define (reset-contract-data)
+    (delete-if-exists require-contracts-file)
+    (delete-if-exists provide-contracts-file))
+  
+  (define (delete-if-exists filename)
+    (when (file-exists? filename)
+      (delete-file filename)))
 
-(define ((intercept-contracts parameter) stx)
-  (displayln stx)
-  (let* ([contracts-box (parameter)]
-         [contracts-list (unbox contracts-box)])
-    (when contracts-list
-      (set-box! contracts-box
-                (cons stx contracts-list)))))
+  (dynamic-wind
+    reset-contract-data
+    (λ ()
+      (typed-module? target)
+      (displayln (get-provide-contracts))
+      (displayln (get-require-contracts)))
+    reset-contract-data))
+
+(define ((intercept-contracts getter setter) stx)
+  (setter (cons (syntax->datum stx)
+                (getter)))
+  stx)
+
+(define ((file->data filename))
+  (if (file-exists? filename)
+      (with-input-from-file filename
+        (λ () (deserialize (read))))
+      '()))
+
+(define ((data->file filename) data)
+  (with-output-to-file filename #:exists 'replace
+    (λ () (write (serialize data)))))
+
+(define-values
+  (get-require-contracts get-provide-contracts)
+  (values (file->data require-contracts-file)
+          (file->data provide-contracts-file)))
+
+(define-values
+  (set-require-contracts set-provide-contracts)
+  (values (data->file require-contracts-file)
+          (data->file provide-contracts-file)))
 
 (define-values
   (intercept-require-contracts intercept-provide-contracts)
-  (values (intercept-contracts require-contracts)
-          (intercept-contracts provide-contracts)))
+  (values (intercept-contracts get-require-contracts set-require-contracts)
+          (intercept-contracts get-provide-contracts set-provide-contracts)))
 
 (define (command-parse argv)
   (command-line
