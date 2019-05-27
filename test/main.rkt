0775511cb7f3a49fb3d8c035e372e0851e6b9744
Cameron Moy <camoy@cs.umd.edu>
2019-05-27 17:38:20 -0400

    Add code to generate benchmark tests

--- test/main.rkt
+++ test/main.rkt
@@ -10,34 +10,37 @@
 
   ;; Module-Path -> Void
   ;; checks to make sure running optimization on the modules does not yield
-  ;; an issue; only runs the first target
-  (define (test-optimize targets)
+  ;; an issue; runs the main target
+  (define (test-optimize main targets)
     (optimize targets
               #:ignore-check #t
               #:verify-off #t)
-    (define main (path->complete-path (first targets)))
-    (in-dir main
-      (check-not-exn (thunk (dynamic-require main #f)))))
+    (define main* (path->complete-path main))
+    (in-dir main*
+      (check-not-exn (thunk (dynamic-require main* #f)))))
+
+  ;; String -> Void
+  ;; takes benchmark name and runs all files with test-optimize
+  (define (test-benchmark benchmark)
+    (let* ([benchmark-dir
+            (build-path "benchmarks" benchmark "typed")]
+           [benchmark-files
+            (directory-list benchmark-dir #:build? benchmark-dir)]
+           [benchmark-files*
+            (filter (negate directory-exists?) benchmark-files)]
+           [is-main?
+            (λ (p) (string=? (path->string (file-name-from-path p))
+                             "main.rkt"))]
+           [main
+            (findf is-main? benchmark-files*)])
+      (test-case
+          benchmark
+        (test-optimize main benchmark-files*))))
 
   (test-case
     "basic programs"
-    (test-optimize '("basic/abs.rkt")))
-
-  (test-case
-    "sieve"
-    (test-optimize '("benchmarks/sieve/typed/main.rkt"
-                     "benchmarks/sieve/typed/streams.rkt")))
-
-  (test-case
-    "snake"
-    (test-optimize '("benchmarks/snake/typed/main.rkt"
-                     "benchmarks/snake/typed/collide.rkt"
-                     "benchmarks/snake/typed/handlers.rkt"
-                     "benchmarks/snake/typed/motion-help.rkt"
-                     "benchmarks/snake/typed/motion.rkt"
-                     "benchmarks/snake/typed/cut-tail.rkt"
-                     "benchmarks/snake/typed/data.rkt"
-                     "benchmarks/snake/typed/data-adaptor.rkt"
-                     "benchmarks/snake/typed/const.rkt")))
-
+    (test-optimize "basic/abs.rkt"
+                   '("basic/abs.rkt")))
+  (test-benchmark "sieve")
+  (test-benchmark "snake")
   )
