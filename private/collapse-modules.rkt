db4286907c349e4229b3a6b9b4f2963d0d683e33
Cameron Moy <camoy@cs.umd.edu>
2019-05-31 16:10:12 -0400

    Topologically sort submodules by their dependency

--- private/collapse-modules.rkt
+++ private/collapse-modules.rkt
@@ -3,32 +3,55 @@
 (provide collapse-modules)
 
 (require racket/function
+         racket/set
+         racket/dict
          racket/path
          racket/list
+         mischief/sort
+         mischief/dict
          syntax/parse)
+
+(define current-dependencies
+  (make-parameter (void)))
 
 ;; [List-of Path] [List-of Syntax] -> Syntax
 ;; collapses modules down to a single one with many submodules
 ;; for consumption by SCV
 (define (collapse-modules targets stxs)
+  (define-values (target->stx target->deps)
+    (for/lists (t->s t->d)
+               ([target targets]
+                [stx stxs])
+      (define-values (stx* deps)
+        (collapse-module target stx))
+      (define target* (string->symbol target))
+      (values (cons target* stx*)
+              (cons target* deps))))
+  (define targets*
+    (topological-sort (map string->symbol targets)
+                      (dict->procedure #:failure (const empty)
+                                       target->deps)))
   #`(module top racket/base
       (#%module-begin
-       #,@(map collapse-module targets stxs))))
+       #,@(map (curry dict-ref target->stx) targets*))))
 
 ;; Path -> Symbol
 ;; converts a path to a symbol
 (define path->symbol
   (compose string->symbol path->string))
 
-;; Path Syntax -> Syntax
-;; collapses a single module down
+;; Path Syntax -> Syntax [List-of Path]
+;; collapses a single module down and returns the paths
+;; it depends on
 (define (collapse-module target stx)
   (syntax-parse stx
     #:datum-literals (module)
     [(module _ lang (mb forms ...))
      (define name* (path->symbol (simplify-path target)))
-     #`(module #,name* lang
-         #,(transform-require target #'(mb forms ...)))]))
+     (parameterize ([current-dependencies (mutable-set)])
+       (values #`(module #,name* lang
+                   #,(transform-require target #'(mb forms ...)))
+               (set->list (current-dependencies))))]))
 
 ;; Path Syntax -> Syntax
 ;; converts require forms to submod form
@@ -41,6 +64,8 @@
        (let* ([complete    (simplify-path (build-path target-dir mod*))]
               [submod-name (path->symbol complete)]
               [dots        (map (const "..") (range depth))])
+         (set-add! (current-dependencies)
+                   (path->symbol complete))
          #`(submod #,@dots #,submod-name))]
       [else mod]))
   (let loop ([depth 1]
