81d44deb26ac684d746ad93b5d97b3cd78c6a3bc
Cameron Moy <camoy@cs.umd.edu>
2019-07-12 21:50:35 -0400

    Add report script

---
+++ etc/report.rkt
@@ -0,0 +1,44 @@
+#lang racket/base
+
+(require racket/list
+         racket/string
+         racket/port
+         racket/match
+         txexpr)
+
+(define data-paths
+  (current-command-line-arguments))
+
+(define (->string x)
+  (with-output-to-string (λ () (display x))))
+
+(define (get-color blame)
+  (match blame
+    [(list 'error _) "red"]
+    [(list x _ ...) "yellow"]
+    [(list) "white"]))
+
+(define (dat->html dat)
+  (define data (call-with-input-file dat read))
+  (define heading
+    (txexpr* 'tr '()
+             (txexpr* 'th '() "Configuration")
+             (txexpr* 'th '() "Performance")
+             (txexpr* 'th '() "Blame")))
+  (define rows
+    (map (λ (r)
+           (define-values (config perf blame)
+             (values (first r) (second r) (third r)))
+           (txexpr* 'tr
+                    (list (list 'bgcolor (get-color blame)))
+                    (txexpr* 'td '() config)
+                    (txexpr* 'td '() (string-join perf "\n"))
+                    (txexpr* 'td '() (string-join (map ->string blame)
+                                                  "\n"))))
+         data))
+
+  (with-output-to-file (path-replace-extension dat #".html") #:exists 'replace
+    (λ () (display (xexpr->html (txexpr 'table '() (cons heading rows)))))))
+
+(for ([p (in-vector data-paths)])
+  (dat->html p))
