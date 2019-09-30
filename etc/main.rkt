fae5c8385310f274a79ee0e796dafdbd37e9421a
Cameron Moy <camoy@cs.umd.edu>
2019-09-29 21:06:54 -0400

    Clean up scripts for the nth time

--- etc/main.rkt
+++ etc/main.rkt
@@ -1,93 +1,173 @@
 #lang racket/base
 
-(require racket/vector
-         racket/file
-         racket/list
-         racket/string
-         racket/path
-         racket/function
-         racket/port
-         racket/tcp
-         "config.rkt"
-         (prefix-in report: "report.rkt")
-         (prefix-in plot: "plot.rkt"))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define BENCHMARKS
+  '("sieve"
+    #;"fsm"
+    "morsecode"
+    #;"zombie"
+    #;"lnm"
+    #;"suffixtree"
+    #;"kcfa"
+    #;"snake"
+    #;"tetris"))
+
+(define CUTOFF 0)
+(define ITERATIONS 1)
+(define NUM-SAMPLES 2)
+(define SAMPLE-FACTOR 1)
+(define BENCHMARK-DIR
+  (vector-ref (current-command-line-arguments) 0))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(require (for-syntax racket/base)
+         racket/runtime-path
+         racket/vector
+         racket/tcp
+         racket/path
+         racket/file
+         basedir
+         "private/report.rkt")
+
+(define blame-box (box '()))
+(define default-bin-dir "/usr/bin")
+(define gtp-dir
+  (writable-data-dir #:program "gtp-measure"))
+(define-runtime-path scv-bin-dir "bin")
+(define-runtime-path scv-measurements-dir
+  (build-path "measurements" "scv"))
+(define-runtime-path baseline-measurements-dir
+  (build-path "measurements" "baseline"))
+
+;; String Boolean -> Vector
+;; Returns vector of correct command line arguments for gtp-measure
+(define (gtp-setup-argv benchmark-dir scv?)
+  (define base-argv
+    (vector "-c" (number->string CUTOFF)
+            "-i" (number->string ITERATIONS)
+            "-S" (number->string SAMPLE-FACTOR)
+            "-R" (number->string NUM-SAMPLES)
+            "--setup" (path->string benchmark-dir)))
+  (if scv?
+      (vector-append (vector "--bin" (path->string scv-bin-dir)) base-argv)
+      (vector-append (vector "--bin" default-bin-dir) base-argv)))
+
+;; Vector ->
+;; Run gtp-measure with argv as command line arguments
 (define (gtp-measure argv)
   (parameterize ([current-command-line-arguments argv]
                  [current-namespace (make-base-empty-namespace)])
     (dynamic-require '(submod gtp-measure/private/raco main) #f)))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Listen for blame information and place in the blame box if you get something
+;; Rinse and repeat
+(define (listen-for-blames!)
+  (define port 8182)
+  (define listener (tcp-listen port))
+  (define (loop)
+    (thread (lambda ()
+              (define-values (in out) (tcp-accept listener))
+              (set-box! blame-box (cons (read in) (unbox blame-box)))
+              (close-input-port in)
+              (close-output-port out)
+              (loop)))
+    (void))
+  (loop))
 
-(define port 8182)
-(define listener (tcp-listen port))
-(define blames (box '()))
-(define (loop)
-  (thread (lambda ()
-            (define-values (in out) (tcp-accept listener))
-            (set-box! blames (cons (read in) (unbox blames)))
-            (close-input-port in)
-            (close-output-port out)
-            (loop)))
-  (void))
-(loop)
+;; String -> Natural
+;; Returns number of modules in the configuration of a given benchmark
+(define (benchmark->modules benchmark-dir)
+  (length (filter (λ (path)
+                    (path-has-extension? path #".rkt"))
+                  (directory-list (build-path benchmark-dir "typed")))))
 
-(define is-out? (curryr path-has-extension? #".out"))
+;; String -> String
+;; Returns bitstring of the fully typed configuration
+(define (benchmark->typed-bitstring benchmark-dir)
+  (make-string (benchmark->modules benchmark-dir) #\1))
 
-(define (get-data benchmark k dir)
-  (define outs (filter is-out? (directory-list dir)))
-  (define path->number
-    (let ([prefix-length
-           (string-length (string-append (number->string k) "-" benchmark))])
-      (λ (p)
-        (define n (substring (path->string (path-replace-extension p #""))
-                             prefix-length))
-        (if (non-empty-string? n) (string->number n) 0))))
-  (define (<* x y)
-    (< (path->number x) (path->number y)))
-  (append-map
-   (λ (x)
-     (define f (open-input-file (build-path dir x)))
-     (let go ([xs '()]
-              [x  (read f)])
-       (if (eof-object? x) (reverse xs) (go (cons x xs) (read f)))))
-   (sort outs <*)))
+;; String -> String
+;; Returns bitstring of the fully untyped configuration
+(define (benchmark->untyped-bitstring benchmark-dir)
+  (make-string (benchmark->modules benchmark-dir) #\0))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(define dats
-  (for/list ([benchmark (in-list benchmarks)]
-             [k         (in-naturals 1)])
-    (set-box! blames '())
-    (displayln (format "Starting: ~a" benchmark))
-    (define benchmark-arg
-      (vector "--setup"
-              (path->string (build-path benchmark-dir benchmark))))
-    (define argv-setup* (vector-append argv-setup benchmark-arg))
-    (define gtp-dir* (build-path gtp-dir (number->string k)))
-    (define argv-resume (vector "--resume" (path->string gtp-dir*)))
-    (unless (directory-exists? gtp-dir*)
-      (gtp-measure argv-setup*))
-    (define outs (filter is-out? (directory-list gtp-dir*)))
-    (for ([out (in-list outs)])
-      (delete-file (build-path gtp-dir* out)))
-    (gtp-measure argv-resume)
-    (define benchmark-assoc
-      (for/list ([data  (get-data benchmark k gtp-dir*)]
-                 [blame (reverse (unbox blames))])
-        (define-values (conf perf)
-          (values (first data)
-                  (second data)))
-        (list conf perf blame)))
-    (define dat-file (build-path "results" (string-append benchmark ".dat")))
-    (with-output-to-file dat-file
-      #:exists 'replace
-      (λ () (write benchmark-assoc)))
-    dat-file))
+(when (directory-exists? gtp-dir)
+  (delete-directory/files gtp-dir))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(listen-for-blames!)
+(for ([benchmark     (in-list BENCHMARKS)]
+      [benchmark-idx (in-naturals 1)])
+  (define benchmark-dir
+    (build-path BENCHMARK-DIR benchmark))
+  (define gtp-measure-benchmark-dir
+    (build-path gtp-dir (number->string benchmark-idx)))
+  (define config-rktd
+    (build-path gtp-measure-benchmark-dir "config.rktd"))
+  (define typed-in
+    (build-path gtp-measure-benchmark-dir
+                (format "0-~a-typed.in" benchmark)))
+  (define untyped-in
+    (build-path gtp-measure-benchmark-dir
+                (format "0-~a-untyped.in" benchmark)))
+  (define blame-report
+    (build-path scv-measurements-dir
+                (format "~a-blames.html" benchmark)))
+  (define resume-argv
+    (vector "--resume" (path->string gtp-measure-benchmark-dir)))
 
-(report:main dats)
-(plot:main)
+  ;; Setup
+  (set-box! blame-box '())
+  (gtp-measure (gtp-setup-argv benchmark-dir #t))
+  (with-output-to-file typed-in
+    (λ () (displayln (benchmark->typed-bitstring benchmark-dir))))
+  (with-output-to-file untyped-in
+    (λ () (displayln (benchmark->untyped-bitstring benchmark-dir))))
+
+  ;; Run (SCV)
+  (gtp-measure resume-argv)
+
+  ;; Generate report
+  (make-directory* scv-measurements-dir)
+  (with-output-to-file blame-report
+    #:exists 'replace
+    (λ ()
+      (benchmark->blame-report benchmark
+                               benchmark-idx
+                               gtp-measure-benchmark-dir
+                               (reverse (unbox blame-box)))))
+
+  ;; Collect measurements (SCV)
+  (for ([path (in-directory gtp-measure-benchmark-dir)]
+        #:when (path-has-extension? path #".out"))
+    (copy-file path
+               (build-path scv-measurements-dir
+                           (file-name-from-path path))
+               #t)
+    (delete-file path))
+
+  ;; Modify bin for baseline
+  (define config-hash
+    (read (open-input-file config-rktd)))
+  (define baseline-argv
+    (vector->list (gtp-setup-argv benchmark-dir #f)))
+  (with-output-to-file config-rktd
+    #:exists 'replace
+    (λ () (write (hash-set config-hash 'argv baseline-argv))))
+
+  ;; Run (baseline)
+  (gtp-measure resume-argv)
+
+  ;; Collect measurements (baseline)
+  (make-directory* baseline-measurements-dir)
+  (for ([path (in-directory gtp-measure-benchmark-dir)]
+        #:when (path-has-extension? path #".out"))
+    (copy-file path
+               (build-path baseline-measurements-dir
+                           (file-name-from-path path))
+               #t)
+    (delete-file path))
+  )
--- etc/results/README.md
+++
@@ -1,7 +0,0 @@
-# Results
-
-This directory will contain results if you run the experiment.
-
-* `html` files will have tables with blame information.
-* `png` files will be overhead plots and lattices.
-* `dat` and `rktd` files contain raw data.
